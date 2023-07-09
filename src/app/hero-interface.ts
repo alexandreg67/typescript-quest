645d6ecb8afef97e32ac0d0b8b273162c9dd14dc
alexandreg67 <alexandre.ag.67@gmail.com>
2023-07-09 15:27:42 +0200

    mise a jour ecran vainqueur

---
+++ src/app/hero-interface.ts
@@ -0,0 +1,10 @@
+export interface HeroInterface {
+    id: number;
+    life: number;
+    power:number;
+    attaqueSpeciale: number;
+    name: string;
+    picture: string;
+    type: string;
+    potion:number;
+}
